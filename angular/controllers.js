e8a8dbb02c6a7bebdcec6ad46d288acf87c313e6
Elliott Regan <regan.elliott@gmail.com>
2013-02-12 17:38:14 -0500

    initial commit

---
+++ angular/controllers.js
@@ -0,0 +1,13 @@
+function ArtistList($scope) {
+  $scope.artists = [
+  {"name": "Sammy J",
+  "instruments": ["vocals","guitar", "piano"],
+  "snippet": "Well, the way they make shows is, they make one show."},
+  {"name": "JC Izzle",
+  "instruments": ["harpsichord", "guitar"],
+  "snippet": "Now that we know who you are, I know who I am."},
+  {"name": "El Bamf™",
+  "instruments": ["guitar"],
+  "snippet": "You know why, David? Because of the kids. They called me Mr Glass."}
+  ];
+};
